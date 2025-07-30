88813e68170dbabcc7e49805c2584f326423f3df
Lucas Daniel Baruja Torales <lucas.baruja@upa.edu.py>
2025-07-30 17:27:17 -0300

    Arreglando firecrawl mediante tool custom

--- src/inverbot_pipeline_dato/main.py
+++ src/inverbot_pipeline_dato/main.py
@@ -3,7 +3,7 @@ import sys
 import warnings
 
 from datetime import datetime
-from .data import data_source
+
 from inverbot_pipeline_dato.crew import InverbotPipelineDato
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
@@ -13,8 +13,7 @@ def run():
     """
     Run the crew.
     """
-    t = data_source.links()
     try:
-        InverbotPipelineDato().crew().kickoff(inputs=t)
+        InverbotPipelineDato().crew().kickoff()
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
